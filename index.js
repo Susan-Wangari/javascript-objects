1e1ad6c973e72d42c1cba0bd24a6d74b093eec73
kareey-commits <56164343+kareey-commits@users.noreply.github.com>
2020-06-17 16:57:42 +0300

    arrays and strings

---
+++ index.js
@@ -0,0 +1,4 @@
+let moreMusic = ["hallelujah", "into your arms", "what a beautiful name"];
+let evenMoreMusic = ["your name is holy", "nobody greater", "none like you"];
+let allMusic = moreMusic.concat(evenMoreMusic);
+console.log(allMusic);
---
+++ templates.js
@@ -0,0 +1,10 @@
+var name = "susan";
+var age = "20";
+var favouriteSnacks =["icecream", "chocolate", "yoghurt"];
+
+const s = "My name is " +  name + ".";
+console.log(s);
+
+// template strings
+const t = `My name is ${name}`;
+console.log(t);
