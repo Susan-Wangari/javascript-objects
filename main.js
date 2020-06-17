db54bb5d29b440c0d2e8f4d6f66b7694906239b8
kareey-commits <56164343+kareey-commits@users.noreply.github.com>
2020-06-17 14:54:44 +0300

    function to get sum

---
+++ main.js
@@ -0,0 +1,11 @@
+function addNumbers(a,b) {
+return a + b;
+}
+console.log(sum(5, 10));
+// let a = 1;
+// for(let i = 0; i<5; i++) {
+//     console.log(i);
+// }
+
+// const b = 2;
+// console.log(b);
