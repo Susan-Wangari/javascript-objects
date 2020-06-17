17040458319c5adf50ee743add4a40782df95c14
kareey-commits <56164343+kareey-commits@users.noreply.github.com>
2020-06-17 14:43:17 +0300

    objects in javascript

---
+++ start.js
@@ -0,0 +1,15 @@
+// objects
+var name = "susan";
+var age = "20";
+var favouriteSnacks =["icecream", "chocolate", "yoghurt"];
+
+var me = {
+    "name": name,
+    "age": age;
+    "favouriteSnacks": favouriteSnacks,
+    "favouritePizza": "Chicken Periperi"
+};
+
+console.log(favouriteSnacks);
+console.log(me["name"]);
+console.log(me.favouritePizza);
